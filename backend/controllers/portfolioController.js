05d81d0b1da7b6938af77bc1b9f5439221243ad3
hamzatalha77 <34461042+hamzatalha77@users.noreply.github.com>
2023-05-09 09:51:17 +0100

    createPortfolio function

--- backend/controllers/portfolioController.js
+++ backend/controllers/portfolioController.js
@@ -5,4 +5,19 @@ const getPortfolios = asyncHandler(async (req, res) => {
   const portfolios = await Portfolio.find({})
   res.json(portfolios)
 })
-export { getPortfolios }
+
+const createPortfolio = asyncHandler(async (req, res) => {
+  try {
+    const portfolio = new Portfolio({
+      name: req.body.name,
+      image: req.body.image,
+      github: req.body.github,
+      live: req.body.live,
+    })
+    const createdPortfolio = await portfolio.save()
+    res.json(201).json(createdPortfolio)
+  } catch (error) {
+    res.json(400).json(error.message)
+  }
+})
+export { getPortfolios, createPortfolio }
