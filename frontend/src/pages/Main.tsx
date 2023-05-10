4424fc6c2d2c0df3bde0e830b232fa9ffa2f5f9b
hamzatalha77 <34461042+hamzatalha77@users.noreply.github.com>
2023-05-10 13:06:48 +0100

    commit

--- frontend/src/pages/Main.tsx
+++ frontend/src/pages/Main.tsx
@@ -4,7 +4,11 @@ const Main = () => {
   const [portfolios, setPortfolios] = useState([])
   return (
     <div className="h-screen w-full bg-slate-400">
-      <div>{}</div>
+      <div>
+        <input type="text" placeholder="Project name..." name="name" />
+        <input type="text" placeholder="Github code source..." name="github" />
+        <input type="text" placeholder="Project Url..." name="live" />
+      </div>
     </div>
   )
 }
