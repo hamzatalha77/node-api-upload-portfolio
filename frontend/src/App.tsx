9a06aa8a66588b067b63b691bb4136503cda53a4
hamzatalha77 <34461042+hamzatalha77@users.noreply.github.com>
2023-05-10 18:44:07 +0100

    commit

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import './main.css'
 import Main from './pages/Main'
+import Home from './pages/Home'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <>
-      <Main />
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" Component={Main}></Route>
+        <Route path="/home" Component={Home}></Route>
+      </Routes>
+    </Router>
   )
 }
 
