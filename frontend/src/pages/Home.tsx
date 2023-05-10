86a76d0ce78799042cdbb5687f48e337c992c286
hamzatalha77 <34461042+hamzatalha77@users.noreply.github.com>
2023-05-11 00:31:43 +0100

    commit

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,7 +1,12 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
+interface Portfolio {
+  _id: string
+  name: string
+  // add more properties as needed
+}
 const Home = () => {
-  const [portfolios, setPortfolios] = useState([])
+  const [portfolios, setPortfolios] = useState<Portfolio[]>([])
   useEffect(() => {
     const fetchProtfolios = async () => {
       const { data } = await axios.get('/api/portfolios')
@@ -13,7 +18,7 @@ const Home = () => {
     <div>
       {portfolios.map((portfolio) => (
         <div key={portfolio._id}>
-          <h1></h1>
+          <h1>{portfolio.name}</h1>
         </div>
       ))}
     </div>
