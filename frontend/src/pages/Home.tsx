0be6d6b11b90a1604fa98c8846e93fe580187e0c
hamzatalha77 <34461042+hamzatalha77@users.noreply.github.com>
2023-05-10 19:09:36 +0100

    commit

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,7 +1,23 @@
-import React from 'react'
-
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 const Home = () => {
-  return <div>Home</div>
+  const [portfolios, setPortfolios] = useState([])
+  useEffect(() => {
+    const fetchProtfolios = async () => {
+      const { data } = await axios.get('/api/portfolios')
+      setPortfolios(data)
+    }
+    fetchProtfolios()
+  }, [])
+  return (
+    <div>
+      {portfolios.map((portfolio) => (
+        <div key={portfolio._id}>
+          <h1></h1>
+        </div>
+      ))}
+    </div>
+  )
 }
 
 export default Home
