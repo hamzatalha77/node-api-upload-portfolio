addd592501c589a0419a9c8658d02855b8cef9af
hamzatalha77 <34461042+hamzatalha77@users.noreply.github.com>
2023-05-11 18:56:50 +0100

    node env

--- backend/server.js
+++ backend/server.js
@@ -19,4 +19,9 @@ app.use('/api/portfolios', portfolioRoutes)
 
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT, console.log(`Server running on port ${PORT}`.yellow.bold))
+app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
+)
