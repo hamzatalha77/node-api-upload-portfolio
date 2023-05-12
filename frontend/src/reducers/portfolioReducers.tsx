d59a40f62fda916470c3d8f299d2fb431f6eeb4c
hamzatalha77 <34461042+hamzatalha77@users.noreply.github.com>
2023-05-12 13:49:18 +0100

    commit

---
+++ frontend/src/reducers/portfolioReducers.tsx
@@ -0,0 +1,26 @@
+import {
+  PORTFOLIOS_CREATE_REQUEST,
+  PORTFOLIOS_CREATE_SUCCESS,
+  PORTFOLIOS_CREATE_FAIL,
+  PORTFOLIOS_CREATE_RESET,
+} from '../constants/portfolioConstants'
+
+interface PortfolioAction {
+  type: string
+  payload?: any
+}
+
+export const portfolioReducer = (state = {}, action: PortfolioAction) => {
+  switch (action.type) {
+    case PORTFOLIOS_CREATE_REQUEST:
+      return { loading: true }
+    case PORTFOLIOS_CREATE_SUCCESS:
+      return { loading: false, success: true, portfolio: action.payload }
+    case PORTFOLIOS_CREATE_FAIL:
+      return { loading: false, error: action.payload }
+    case PORTFOLIOS_CREATE_RESET:
+      return {}
+    default:
+      return state
+  }
+}
