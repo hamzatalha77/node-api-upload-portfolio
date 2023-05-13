ceeb73304af97e0482f77b550fa2518b49904319
hamzatalha77 <34461042+hamzatalha77@users.noreply.github.com>
2023-05-13 19:06:33 +0100

    commit

--- frontend/src/actions/portfolioActions.ts
+++ frontend/src/actions/portfolioActions.ts
@@ -3,16 +3,28 @@ import axios from 'axios'
 import {
   PORTFOLIOS_CREATE_REQUEST,
   PORTFOLIOS_CREATE_SUCCESS,
-  // PORTFOLIOS_CREATE_FAIL,
+  PORTFOLIOS_CREATE_FAIL,
 } from '../constants/portfolioConstants'
 
-export const createPortfolio = () => async (dispatch: Dispatch) => {
-  dispatch({
-    type: PORTFOLIOS_CREATE_REQUEST,
-  })
-  const { data } = await axios.get(`api/portfolios`, {})
-  dispatch({
-    type: PORTFOLIOS_CREATE_SUCCESS,
-    payload: data,
-  })
-}
+export const createPortfolio =
+  (portfolioData: any) => async (dispatch: any) => {
+    try {
+      dispatch({ type: PORTFOLIOS_CREATE_REQUEST })
+
+      // Make API call using axios
+      const { data } = await axios.post('/api/portfolios', portfolioData)
+
+      dispatch({
+        type: PORTFOLIOS_CREATE_SUCCESS,
+        payload: data,
+      })
+    } catch (error) {
+      dispatch({
+        type: PORTFOLIOS_CREATE_FAIL,
+        payload:
+          error.response && error.response.data.message
+            ? error.response.data.message
+            : error.message,
+      })
+    }
+  }
