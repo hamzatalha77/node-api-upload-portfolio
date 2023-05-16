8bc137c977a81d00d3d0ec7b9eaa30a5c0b5c444
hamzatalha77 <34461042+hamzatalha77@users.noreply.github.com>
2023-05-16 13:55:13 +0100

    commit

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 import dotenv from 'dotenv'
+import path from 'path'
 import express from 'express'
 import connectDB from './config/db.js'
 import colors from 'colors'
 import portfolioRoutes from './routes/portfolioRoutes.js'
 import { errorHandler, notFound } from './middleware/errorMiddleWare.js'
+import uploadRoutes from './routes/uploadRoutes.js'
 dotenv.config()
 
 connectDB()
@@ -12,11 +14,23 @@ const app = express()
 
 app.use(express.json())
 
-app.get('/', (req, res) => {
-  res.send('Api is running...')
-})
-
 app.use('/api/portfolios', portfolioRoutes)
+app.use('/api/upload', uploadRoutes)
+
+const __dirname = path.resolve()
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  )
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running....')
+  })
+}
 
 app.use(notFound)
 app.use(errorHandler)
